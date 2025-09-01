b3301c59263e818a8c2a5f2cd6664de395422c90
ZPC-dsg <zpcdsg@gmail.com>
2025-09-01 23:22:04 +0800

    Prepare for OIT logic

--- Effects/OIT.cpp
+++ Effects/OIT.cpp
@@ -1,5 +1,12 @@
 #include "OIT.h"
 
+#include <SceneGraph/modelproxy.h>
+#include <SceneGraph/drawableproxy.h>
+#include <Bindables/imagetexture2D.h>
+#include <assimploader.h>
+#include <drawableloader.h>
+#include <Bindables/shaderprogram.h>
+
 namespace OITEffects
 {
 	OIT::OIT(const std::string& name)
@@ -11,15 +18,61 @@ namespace OITEffects
 
 	void OIT::render()
 	{
-
+		render_opaque();
+		render_OIT();
+		composite();
 	}
 
 	void OIT::prepare()
 	{
-
+		prepare_opaque();
+		prepare_transparent();
+		prepare_OITdata();
 	}
 
 	void OIT::prepare_ui(const std::string& name)
+	{
+		
+	}
+
+	void OIT::prepare_opaque()
+	{
+		m_proxy = AssimpLoader::LoadModel("Sponza", "sponza.obj", m_main_scene);
+		GLuint vertex = Bind::ShaderObject::Resolve(Bind::ShaderObject::ShaderType::Vertex, "vertex", "test", "model_test.vert");
+		GLuint fragment = Bind::ShaderObject::Resolve(Bind::ShaderObject::ShaderType::Fragment, "fragment", "test", "model_test.frag");
+		m_opaque_shader = Bind::ShaderProgram::Resolve("shader", std::vector<GLuint>{vertex, fragment});
+		m_proxy->AddRootBindable(m_opaque_shader);
+		std::vector<DrawItems::VertexType> instruction{ DrawItems::VertexType::Position,DrawItems::VertexType::Normal,DrawItems::VertexType::Texcoord };
+		m_proxy->AddRootVertexRule(instruction);
+		m_proxy->AddRootTextureRule("texture_diffuse1", 0, SceneGraph::Material::TextureCategory::DIFFUSE);
+		m_proxy->AddRootTextureRule("texture_specular1", 1, SceneGraph::Material::TextureCategory::SPECULAR);
+		m_proxy->AddRootUniformRule<SceneGraph::ConfigurationType::Transformation>(m_opaque_shader->EditUniform("model").GetLeafUniform(),
+			[](glm::mat4 model)->glm::mat4 {return model; });
+		m_proxy->Cook();
+		m_proxy->ScaleModel(glm::vec3(0.02f));
+	}
+
+	void OIT::prepare_transparent()
+	{
+
+	}
+
+	void OIT::prepare_OITdata()
+	{
+
+	}
+
+	void OIT::render_opaque()
+	{
+
+	}
+
+	void OIT::render_OIT()
+	{
+
+	}
+
+	void OIT::composite()
 	{
 
 	}
--- Effects/OIT.h
+++ Effects/OIT.h
@@ -2,6 +2,16 @@
 
 #include <utils.h>
 
+namespace SceneGraph
+{
+	class ModelProxy;
+}
+
+namespace Bind
+{
+	class ShaderProgram;
+}
+
 namespace OITEffects
 {
 	class OIT :public Utils
@@ -15,8 +25,17 @@ namespace OITEffects
 		void prepare_ui(const std::string& name) override;
 
 	private:
+		void prepare_opaque();
+		void prepare_transparent();
+		void prepare_OITdata();
+
+		void render_opaque();
+		void render_OIT();
+		void composite();
 
 	private:
+		std::shared_ptr<SceneGraph::ModelProxy> m_proxy;
 
+		std::shared_ptr<Bind::ShaderProgram> m_opaque_shader;
 	};
 }
